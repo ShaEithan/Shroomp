dd2748fded92f09acc6624b466ae8abf678184e7
Rodolfo Chavez <Chavezrodolfo522@gmail.com>
2022-10-10 11:27:42 -0500

    added frog taking health using dot handler so that it changes color

--- Assets/Scripts/AiPatrol.cs
+++ Assets/Scripts/AiPatrol.cs
@@ -15,18 +15,38 @@ public class AiPatrol : MonoBehaviour
     public Rigidbody2D rb;
     public Transform groundCheckPos;
     public LayerMask groundLayer;
+
+    private bool isInvincible = false;
+    private float invincibleTime;
+    private float dotTime, dotInvulTime;
+    private bool dotActive;
+    private bool dotIsInvul = false;
+    public float dotInvulSetter = 0.5f;
+    public int fireWeakness = 1;
+    public int iceWeakness = 1;
+    public float maxVelocity = 6;
+    private StatusEffectController statusHandler;
+    Vector4 colorStorage;
+
+    SpriteRenderer frogSprite;
     void Start()
     {
         mustPatrol = true;
+        currentHealth = maxHealth;
+        frogSprite = GetComponent<SpriteRenderer>();
+        colorStorage = frogSprite.color;
+        statusHandler = FindObjectOfType<StatusEffectController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("changing health new health " + currentHealth);
         if (mustPatrol)
         {
             Patrol();
         }
+        dotHandler();
     }
 
     private void FixedUpdate()
@@ -54,15 +74,94 @@ public class AiPatrol : MonoBehaviour
         mustPatrol = true;
     }
 
-    void OnTriggerStay2D(Collider2D collision)
-    {
-        Debug.Log("Trigger");
-        ShroompController player = collision.gameObject.GetComponent<ShroompController>();
+    private bool hitOnce;
+    public int bombDamage = 20;
 
-        if (player != null)
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Frog Collided With " + collision.tag);
+        //hitOnce = false;
+        if (collision.gameObject.CompareTag("Player"))
         {
-            player.ChangeHealth(-1);
+            ShroompController player = collision.gameObject.GetComponent<ShroompController>();
+            if (player != null && !player.isDashing)
+            {
+                player.ChangeHealth(-1);
+            }
+            if (player.isDashing && !isInvincible)
+            {
+
+                invincibleTime = 0.2f;
+                isInvincible = true;
+                ChangeHealth(-1);
+                if (currentHealth > 0)
+                {
+                    dotTime = statusHandler.getDotTime();
+                    dotActive = true;
+                }
+            }
         }
+        if (collision.gameObject.CompareTag("Bomb"))
+        {
+            ChangeHealth(-bombDamage);
+        }
+
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        if (currentHealth == 0)
+            Destroy(transform.gameObject);
+    }
+
+    private float colorChangeDelay;
+    public float colorChangeSetter = 0.1f;
+    private bool isColorChanging = true;
+
+    void dotHandler()
+    {
+        if (dotActive && !dotIsInvul)
+        {
+
+            if (statusHandler.fireUp)
+            {
+                frogSprite.color = new Color(1f, 0f, 0f, 1f); 
+                ChangeHealth(-5*fireWeakness);
+                dotInvulTime = dotInvulSetter;
+                dotIsInvul = true;
+                colorChangeDelay = colorChangeSetter;
+                isColorChanging = true;
+            }
+
+
+        }
+        if(dotActive)
+        {
+            if (statusHandler.iceUp)
+            {
+                colorChangeDelay -= Time.deltaTime;
+                if (colorChangeDelay < 0)
+                frogSprite.color = new Color(0f, 0f, 1f, 1f);
+                maxVelocity = Mathf.Clamp(6-iceWeakness, 1, 6);
+            }
+            dotTime -= Time.deltaTime;
+            if (dotTime < 0)
+            {
+                dotActive = false;
+                //Restore max velocity and then color
+                maxVelocity = 6f;
+                frogSprite.color = colorStorage;
+            }
+        }
+        if(dotIsInvul)
+        {
+            dotInvulTime -= Time.deltaTime;
+            if (dotInvulTime < 0)
+                dotIsInvul = false;
+        }
+
     }
 
 
