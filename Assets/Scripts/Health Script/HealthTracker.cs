377a315c413b291dbb39b992f5537ef483bc7ae3
LAceve <laaceve3@CougarNet.UH.EDU>
2022-10-30 21:17:46 -0500

    Bat Damage and Dead
    Also extra stuff for health tracking for next push

---
+++ Assets/Scripts/Health Script/HealthTracker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTracker : MonoBehaviour
+{
+    public Sprite fullHeart, halfHeart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
